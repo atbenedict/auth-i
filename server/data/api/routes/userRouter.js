afd4771c86f1549c11b66ea749711042cf299601
Andrew Benedict <atbenedict@gmail.com>
2019-02-25 17:16:39 -0500

    set up api/authorization/file structure

---
+++ server/data/api/routes/userRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const helper = require("../helpers/userHelpers");
+
+router.get("/", (req, res) => {
+  helper
+    .getUsers()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json({ errorMessage: "cant receive users" }));
+});
+
+module.exports = router;
